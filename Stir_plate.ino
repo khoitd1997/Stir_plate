3df1f7927e6e1e41e065947d577826c16c4579cd
F1 race car team <ucichemecars@gmail.com>
2017-09-07 21:22:26 -0700

    Initial blank code

---
+++ Stir_plate.ino
@@ -0,0 +1,12 @@
+//Use pin 9 because it will only affect the servo function
+//Pin 9 base frequency is 31250 Hz
+//http://forum.arduino.cc/index.php?topic=18742.0 Calculated values for constants
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
