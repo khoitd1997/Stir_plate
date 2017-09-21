7ba2fb50b09416ba9610d85e6955ee82f3d1c427
F1 race car team <ucichemecars@gmail.com>
2017-09-21 00:35:33 -0700

    Remove delay

--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -1,10 +1,12 @@
 
-#define BUZZER_PIN 9
+#define BUZZER_PIN 9//PWM pin that controls the buzzer
 #define TEMPO 65
-#define SONG_LENGTH 50
+#define SONG_LENGTH 50//More beep if bigger number
 #define BEATS 2
-#define DURATION BEATS*TEMPO
+#define DURATION BEATS*TEMPO//duration of each note played
 #define FREQUENCY 523 //pick from 262, 294, 330, 349, 392, 440, 494, 523
+#define ALARM_ON 1//1 for sounding alarm 0 for not
+
 void setup()
 {
         pinMode(BUZZER_PIN, OUTPUT);
@@ -13,17 +15,21 @@ void setup()
 
 void loop()
 {
+static unsigned long start_time=millis();//millis() may overflow but the device is expected to operate only for short DURATION
 
+if((millis()-start_time>DURATION+TEMPO/10) && ALARM_ON){
 play_alarm();
+start_time=millis();
+}
 
-delay(5000);
 }
 
 void play_alarm(void){
 
-  for ( unsigned int i = 0; i < SONG_LENGTH; i++)
-  {
-          i%2?delay(DURATION):tone(BUZZER_PIN, FREQUENCY, DURATION), delay(DURATION);
-          delay(TEMPO/10);  // brief pause between notes
-}
+  static unsigned int i=0;//doesn't matter if i overflow
+if(i++%2!=0)
+
+tone(BUZZER_PIN, FREQUENCY, DURATION);
+
+
 }
