b7dd0edaa8f1cfdbb069155176b6f7ea6aab9521
F1 race car team <ucichemecars@gmail.com>
2017-09-23 02:52:45 -0700

    Clean up the code
    Added comments about the registers and delete the serial stuffs

--- Stir_plate/Stir_plate.ino
+++ Stir_plate/Stir_plate.ino
@@ -5,7 +5,7 @@
 #define BIT(x) (0x01 << (x))
 
 //potentiometer must be at A3, hardcoded into the value of the register
-#define PWM_FAN_PIN 10//fans that feed the PWM value to the fans
+#define PWM_FAN_PIN 10//fans that feed the PWM value to the fans, must be a PWM pin
 #define FAN_ON_OFF_PIN 4//turn on or off the transistor that controls the fan
 
 
@@ -19,42 +19,38 @@ void setup()
         DDRD=0x10;// enable the PWM output (you now have a PWM signal on digital pin 10)
         DDRB=0x04;//enable the digital output on pin 4
         PORTD=0x10;//set pin 4 to HIGH
-        Serial.begin(9600);
 }
 void loop() {
 
-        int PWMValue=map(constrain(Read_Potentiometer(),0,932),0, 932, 50, 400);
-        Serial.print(Read_Potentiometer());
-        Serial.print("      ");
-        Serial.println(PWMValue);
-        if(PWMValue>75)
+        int PWMValue=map(constrain(Read_Potentiometer(),0,932),0, 932, 50, 400);//map the PWM value into a certain range from the potentiometer value
+        if(PWMValue>75)//threshold that the potentiometer will actually impact the fan speed
         {
-                PORTD=0x10;
+                PORTD=0x10;//turn on the plate
                 OCR1B=PWMValue;
         }
-        else PORTD=0x00;  //set pin 4 to LOW
+        else PORTD=0x00;//turn off the plate
 
 
 }
 int Read_Potentiometer()
 {
-  //
-
-//bit_set(DIDR0,BIT(3));
+  //select reference as AVcc with external capacitor at AREF
         bit_set(ADMUX,BIT(6));
         bit_clear(ADMUX, BIT(7));
 
+//select ADC 3 or analogpin 3 on the Arduino
         bit_clear(ADMUX, BIT(3));
         bit_clear(ADMUX, BIT(2));
         bit_set(ADMUX, BIT(1));
         bit_set(ADMUX, BIT(0));
 
+//turn off the auto trigger and then initiate the ADC
         bit_clear(ADCSRA,BIT(5));
         bit_set(ADCSRA,BIT(7));
         bit_set(ADCSRA,BIT(6));//stay high as long as conversion in progress
 
 
-        while(ADSC==0b1) {
+        while(ADSC==0b1) {//check if conversion is done
         }
 
         return ADC;
