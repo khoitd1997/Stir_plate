a447e517dbdef9b92bec13061466f218b2472e68
F1 race car team <ucichemecars@gmail.com>
2017-09-23 02:47:08 -0700

    Combine with original PWM code, tested working with the stirplate and potentiometer

--- Stir_plate/Stir_plate.ino
+++ Stir_plate/Stir_plate.ino
@@ -4,9 +4,10 @@
 #define bit_clear(p,m) ((p) &= ~(m))
 #define BIT(x) (0x01 << (x))
 
-#define SPEED_CONTROL_PIN A5//Pins that read the value of the potentiometer
+//potentiometer must be at A3, hardcoded into the value of the register
 #define PWM_FAN_PIN 10//fans that feed the PWM value to the fans
 #define FAN_ON_OFF_PIN 4//turn on or off the transistor that controls the fan
+
 
 
 void setup()
@@ -22,46 +23,39 @@ void setup()
 }
 void loop() {
 
-        // int PWMValue=map(constrain(analogRead(SPEED_CONTROL_PIN),0,932),0, 932, 50, 400);
-        // Serial.print(analogRead(SPEED_CONTROL_PIN));
-        // Serial.print("      ");
-        // Serial.println(PWMValue);
-        // if(PWMValue>75)
-        // {
-        //         PORTD=0x10;
-        //         OCR1B=PWMValue;
-        // }
-        // else PORTD=0x00;  //set pin 4 to LOW
-        //real value about 400
-         Serial.print(Read_Potentiometer(), DEC);
-         Serial.print("    ");
-         Serial.println(analogRead(6),DEC);
+        int PWMValue=map(constrain(Read_Potentiometer(),0,932),0, 932, 50, 400);
+        Serial.print(Read_Potentiometer());
+        Serial.print("      ");
+        Serial.println(PWMValue);
+        if(PWMValue>75)
+        {
+                PORTD=0x10;
+                OCR1B=PWMValue;
+        }
+        else PORTD=0x00;  //set pin 4 to LOW
+
+
 }
 int Read_Potentiometer()
 {
   //
 
 //bit_set(DIDR0,BIT(3));
-      bit_set(ADMUX,BIT(6));
-      bit_clear(ADMUX, BIT(7));
+        bit_set(ADMUX,BIT(6));
+        bit_clear(ADMUX, BIT(7));
 
         bit_clear(ADMUX, BIT(3));
         bit_clear(ADMUX, BIT(2));
         bit_set(ADMUX, BIT(1));
         bit_set(ADMUX, BIT(0));
 
-        // bit_set(ADCSRA, BIT(2));
-        // bit_set(ADCSRA, BIT(1));
-        // bit_set(ADCSRA, BIT(0));
-
         bit_clear(ADCSRA,BIT(5));
         bit_set(ADCSRA,BIT(7));
         bit_set(ADCSRA,BIT(6));//stay high as long as conversion in progress
-        //result written to ADC data register ADIF is set
-        //ADC=Vin*1024/Vref
-        //use ADC3
+
+
         while(ADSC==0b1) {
         }
-        //0-3
+
         return ADC;
 }
